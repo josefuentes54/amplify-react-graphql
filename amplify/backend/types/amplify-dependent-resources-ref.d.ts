03df51ac81999598579f41179dcb6ac876c91882
Jose Ignacio Fuentes Osorio <jose.fuentes54@inacapmail.cl>
2024-09-25 22:09:41 -0300

    Actualización del frontend para gestionar notas

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphql3943231d": {
       "AppClientID": "string",
